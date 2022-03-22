e9410209e5c75dfbaa59fe773f707dea32860e5e
Fchacin <100698787+Fchacin@users.noreply.github.com>
2022-03-22 07:54:46 -0400

    Add files via upload

---
+++ pagina/interfaz 1.py
@@ -0,0 +1,58 @@
+﻿from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+def InfoAdicional():
+    messagebox.showinfo("Acerca de nuestro proyecto","Nuestro proyecto esta desarrollado mediante software libre, no infrige derechos de autor ni copyright")
+
+def InfoEquipo():
+    messagebox.showwarning("Nosotros", "Este programa fue hecho en equipo")
+
+def salirdelapp():
+    valor=messagebox.askquestion("Salir", "¿Desea salir de la aplicacion?") 
+    if valor=="yes":
+       root.destroy() 
+
+def Abrirarchivo():
+    archivo=filedialog.askopenfilename(filetypes=(
+        ("Ficheros en PDF","*.PDF"),
+        ("Todos los ficheros","*.*")
+    ), 
+    title = "Abrir documento"
+)     
+    print(archivo)
+
+root = Tk()
+
+root.geometry("800x600")
+
+root.iconbitmap("volante.ico")
+
+root.title("Parking System")
+
+menubar = Menu(root)
+root.config(menu=menubar)
+
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Nuevo")
+filemenu.add_command(label="Abrir", command=Abrirarchivo)
+filemenu.add_command(label="Guardar")
+filemenu.add_command(label="Cerrar")
+filemenu.add_separator()
+filemenu.add_command(label="Salir", command=salirdelapp)
+
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Cortar")
+editmenu.add_command(label="Copiar")
+editmenu.add_command(label="Pegar")
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Nosotros", command=InfoEquipo)
+helpmenu.add_separator()
+helpmenu.add_command(label="Acerca de...", command=InfoAdicional)
+
+menubar.add_cascade(label="Archivo", menu=filemenu)
+menubar.add_cascade(label="Editar", menu=editmenu)
+menubar.add_cascade(label="Ayuda", menu=helpmenu)
+
+root.mainloop()
